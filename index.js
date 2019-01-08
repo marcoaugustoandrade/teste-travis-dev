49e8de7b198e77625e50f3c04f46b40eb0a98111
Marco Antonio Augusto de Andrade <marco.andrade@ifro.edu.br>
2019-01-08 14:55:50 -0400

    Corrigindo ponto-e-vírgula

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Testando o Travis-ci\n')
+  res.end('Testando o Travis-ci\n');
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
